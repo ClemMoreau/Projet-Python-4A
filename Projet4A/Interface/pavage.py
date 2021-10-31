98c1a492b750edb710d4b60ab73dbf08803c3c57
Papy92 <clement.moreau92@gmail.com>
2021-10-31 17:11:36 +0100

    Modif triangle et intégration déformation V1
    Du coup triangle je me suis rendu compte que tu dessinais des triangles pluiseurs fois du coup j'ai simplifier tous ça
    Pour la déformatino c'est nickel sur le triangle, faut que je fasse une modif pour les carrés

--- Projet4A/Interface/pavage.py
+++ Projet4A/Interface/pavage.py
@@ -20,23 +20,27 @@ class Pavage(QtWidgets.QWidget):
         self.numberPolygonPerColumn = self.size[0]//self.length
         self.typeofPolygon = typeofPolygon
         self.listOfPolygon = []        
+        
+        self.point_to_add = False
+        self.first_time = True
       
     #evenement QPaintEvent
     def paintEvent(self, event):   # event de type QPaintEvent
      
         # recupere le QPainter du widget
-        
-        if (self.typeofPolygon == "Square"):
+        if (self.typeofPolygon == "Square" and self.first_time):
             self.draw_rect()  
-        if (self.typeofPolygon == "Triangle"):
+        if (self.typeofPolygon == "Triangle" and self.first_time):
             self.draw_triangle()
-        if (self.typeofPolygon == "Hexagon"):
+        if (self.typeofPolygon == "Hexagon" and self.first_time):
             self.draw_hexagon()
-        return
+            
+        painter = QtGui.QPainter(self)
+        for poly in self.listOfPolygon:
+            painter.drawPolygon(poly)
+            
     
     def draw_rect(self):
-        
-            painter = QtGui.QPainter(self)
             
             for i in range(-1, (self.numberPolygonPerLine + 1)):
             
@@ -62,57 +66,67 @@ class Pavage(QtWidgets.QWidget):
                         #Bot-left point
                         polygon << QtCore.QPointF(abscissa, self.length + ordinate)
                         
-                        painter.drawPolygon(polygon)
-                        
                         self.listOfPolygon.append(polygon)
                     paint = not paint
+                    
+            self.first_time = False
         
     def draw_triangle(self):
         
-        painter = QtGui.QPainter(self)  
-        
-        for i in range(-1,self.numberPolygonPerLine+1):
+        point = QtCore.QPointF(-0.5 * self.length, -TRIANGLE_CONST * self.length)
+        for j in range(0,self.numberPolygonPerColumn+2):               
             
-            for j in range(-1,self.numberPolygonPerColumn+1):               
-            
-                iLength = i * self.length
-                jLength = j * self.length
-
-                #Si j est pair, on dessine des triangles oreintés vers le bas
-                if (j % 2 == 0) :
-                    
-                    polygon = QtGui.QPolygon()
-                            
-                    polygon << QtCore.QPoint(iLength, jLength * TRIANGLE_CONST)
-                    polygon << QtCore.QPoint(self.length + iLength, jLength * TRIANGLE_CONST)
-                    polygon << QtCore.QPoint(0.5 * self.length + iLength, (jLength + self.length) * TRIANGLE_CONST)
-                    painter.drawPolygon(polygon)
-                    
-                else :
-                    #Si j est impair on  dessine des trainlge orienté vers le haut
-                    polygon = QtGui.QPolygon()
-                    
-                    polygon << QtCore.QPoint(0.5 * self.length + iLength, jLength * TRIANGLE_CONST)
-                    polygon << QtCore.QPoint(iLength, (jLength + self.length) * TRIANGLE_CONST)
-                    polygon << QtCore.QPoint(self.length + iLength, (jLength + self.length) * TRIANGLE_CONST)
-                    painter.drawPolygon(polygon)
-                    
-                self.listOfPolygon.append(polygon)
+            for i in range(-1,self.numberPolygonPerLine+2):
                 
-                #A chaque itération on dessine les segments qui finissent les triangles non tracés
-                polygon = QtGui.QPolygon()
-                polygon << QtCore.QPoint(0, jLength * TRIANGLE_CONST)
-                polygon << QtCore.QPoint(0.5 *self.length, jLength * TRIANGLE_CONST)
-                painter.drawPolygon(polygon)
+                polygon = QtGui.QPolygonF()
+                polygon << QtCore.QPoint(point.x(), point.y())
+                polygon << QtCore.QPoint(point.x() + 0.5 * self.length, point.y() + TRIANGLE_CONST * self.length)
+                polygon << QtCore.QPoint(point.x() - 0.5 * self.length, point.y() + TRIANGLE_CONST * self.length)
+                
+                point.setX(point.x() + self.length)
+                    
+                if(i == -1 and j%2==0):
+                    point_mem = polygon.at(2)
+                elif(i == -1 and j%2==1):
+                    point_mem  = polygon.at(1)
                 
                 self.listOfPolygon.append(polygon)
-
                 
-                polygon = QtGui.QPolygon()
-                polygon << QtCore.QPoint(0.5 * self.length + iLength, (jLength + self.length) * TRIANGLE_CONST)
-                polygon << QtCore.QPoint(1.5 * self.length + iLength, (jLength + self.length) * TRIANGLE_CONST)
-                painter.drawPolygon(polygon)
-                self.listOfPolygon.append(polygon)
+            point = point_mem
+        self.first_time = False
                     
     def draw_hexagon(self):
         painter = QtGui.QPainter(self)  
+        
+        self.first_time = False
+        
+    """DEBUT DE DEFORMATION NE MARCHE BIEN QU'AVEC LES TRIANGLES
+    AVEC LES CARRES çA MARCHE MAIS POUR 1 TRAIT SUR 2 ET C'EST NORMAL VU QU'ON DESSIN LA MOITIE DES CARRES"""    
+    def mouseReleaseEvent(self, event): # evenement mouseRelease
+        if(self.point_to_add):  
+            vector = (event.pos().x() - self.listOfPolygon[self.poly].at(self.indice - 1).x(), event.pos().y() - self.listOfPolygon[self.poly].at(self.indice - 1).y())  
+            for j in range(0,len(self.listOfPolygon) - 1):
+               point = QtCore.QPointF(vector[0] + self.listOfPolygon[j].at((self.indice - 1)).x(),vector[1] + self.listOfPolygon[j].at(self.indice - 1).y())
+               self.listOfPolygon[j].insert(self.indice,point)
+               
+            self.update()
+            self.point_to_add = False
+        
+       
+    def mousePressEvent(self, event): # evenement mousePress
+        self.pStart = event.pos()
+        
+        for j in range(0,len(self.listOfPolygon)):
+            for i in range (0,self.listOfPolygon[j].count() + 1):
+                if (self.point_to_add == False):
+                   
+                    
+                    X = (min(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).x() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).x() + 10),
+                        max(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).x() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).x() + 10))
+                    Y = (min(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).y() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).y() + 10),
+                        max(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).y() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).y() + 10))
+                    
+                    if(X[0] < self.pStart.x() < X[1] and Y[0] < self.pStart.y() < Y[1]):
+                        self.indice = i + 1
+                        self.poly = j
+                        self.point_to_add = True
--- test/ContainTest.py
+++ test/ContainTest.py
@@ -8,31 +8,60 @@ Created on Fri Oct 22 23:10:12 2021
 from PyQt5 import QtWidgets, QtGui, QtCore, Qt
 import math
 
+TRIANGLE_CONST = math.sqrt(3)/2
+
 class Contain(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setMouseTracking(True) # on active le mouseTracking
         self.cursorPos = None
-        self.numberPolygonPerLine = 52
+        self.numberPolygonPerLine = 12
         self.length = 1600//self.numberPolygonPerLine
         self.numberPolygonPerColumn = 1600//self.length
         self.listOfPolygon = []
+        
         self.add = False
         self.first = True
+        
+    def draw_triangle(self):
+        if(self.first):
+            point = QtCore.QPointF(-0.5 * self.length, -TRIANGLE_CONST * self.length)
+            for j in range(0,self.numberPolygonPerColumn+2):               
+                
+                for i in range(-1,self.numberPolygonPerLine+2):
+                
+                    iLength = i * self.length
+                    jLength = j * self.length
+                    
+                    polygon = QtGui.QPolygonF()
+                    polygon << QtCore.QPoint(point.x(), point.y())
+                    polygon << QtCore.QPoint(point.x() + 0.5 * self.length, point.y() + TRIANGLE_CONST * self.length)
+                    polygon << QtCore.QPoint(point.x() - 0.5 * self.length, point.y() + TRIANGLE_CONST * self.length)
+                    
+                    point.setX(point.x() + self.length)
+                        
+                    if(i == -1 and j%2==0):
+                        point_mem = polygon.at(2)
+                    elif(i == -1 and j%2==1):
+                        point_mem  = polygon.at(1)
+                    
+                    self.listOfPolygon.append(polygon)
+                    
+                point = point_mem        
+            self.first = False
+                    
         
     def draw_rect(self):
         if(self.first):
             #painter = QtGui.QPainter(self)
             
-            for i in range(-1, (self.numberPolygonPerLine + 2)):
-            
-                
-                if (i % 2 == 0):
+            for j in range(-1, (self.numberPolygonPerColumn + 1)):
+                if (j % 2 == 0):
                     paint = True
                 else:
                     paint = False
-                    
-                for j in range(-1, (self.numberPolygonPerColumn + 1)):
+                        
+                for i in range(-1, (self.numberPolygonPerLine + 1)):
                     if(paint):
                         polygon = QtGui.QPolygonF()
                         
@@ -55,29 +84,29 @@ class Contain(QtWidgets.QWidget):
                     self.first = False
         
     def paintEvent(self, event):
-       self.draw_rect()
+       self.draw_triangle()
        painter = QtGui.QPainter(self)
        for poly in self.listOfPolygon:
            painter.drawPolygon(poly)
     
     def mouseReleaseEvent(self, event): # evenement mouseRelease
         if(self.add):  
-            
-            
-            self.listOfPolygon[self.poly].insert(self.indice,event.pos())
-            for i in range (0,self.listOfPolygon[self.poly].count()):
-                print(i ," ", self.listOfPolygon[self.poly].at(i))
+            vector = (event.pos().x() - self.listOfPolygon[self.poly].at(self.indice - 1).x(), event.pos().y() - self.listOfPolygon[self.poly].at(self.indice - 1).y())  
+            for j in range(0,len(self.listOfPolygon) - 1):
+               point = QtCore.QPointF(vector[0] + self.listOfPolygon[j].at((self.indice - 1)).x(),vector[1] + self.listOfPolygon[j].at(self.indice - 1).y())
+               self.listOfPolygon[j].insert(self.indice,point)
+               
             self.update()
             self.add = False
         
        
     def mousePressEvent(self, event): # evenement mousePress
         self.pStart = event.pos()
-        print("press: ", self.pStart) 
         
         for j in range(0,len(self.listOfPolygon)):
             for i in range (0,self.listOfPolygon[j].count() + 1):
                 if (self.add == False):
+                   
                     
                     X = (min(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).x() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).x() + 10),
                         max(self.listOfPolygon[j].at(i % self.listOfPolygon[j].count()).x() - 10,self.listOfPolygon[j].at((i+1)% self.listOfPolygon[j].count()).x() + 10))
@@ -87,7 +116,6 @@ class Contain(QtWidgets.QWidget):
                     if(X[0] < self.pStart.x() < X[1] and Y[0] < self.pStart.y() < Y[1]):
                         self.indice = i + 1
                         self.poly = j
-                        print("depuis press ",i)
                         self.add = True
                     
            
